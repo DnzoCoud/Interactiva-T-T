d399620fda7c945adeaa8a69865c8579df6c46b9
Daniel Rodriguez <dangelsfocus@gmail.com>
2025-01-17 16:51:51 -0500

    feat: user model, abtract to django user model

--- backend/prueba/modules/users/models.py
+++ backend/prueba/modules/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, cedula, password=None, **extra_fields):
+        if not cedula:
+            raise ValueError("El usuario debe tener una cédula")
+        user = self.model(cedula=cedula, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, cedula, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(cedula, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    cedula = models.CharField(max_length=20, unique=True)
+    username = models.CharField(max_length=20, unique=True, default=None)
+    objects = CustomUserManager()
+    USERNAME_FIELD = "username"
+
+    def __str__(self):
+        return self.cedula
