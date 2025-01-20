86802739e3943bcb50c67c8ac022e3c316aa9faa
Daniel Rodriguez <dangelsfocus@gmail.com>
2025-01-20 08:25:15 -0500

    feat: Create Serializer and make user create view

---
+++ backend/prueba/modules/users/serializers.py
@@ -0,0 +1,9 @@
+from modules.users.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["cedula", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
--- backend/prueba/modules/users/views.py
+++ backend/prueba/modules/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from pydantic import ValidationError as PydanticValidationError
+from modules.users.dtos import UserCreateDto
+from rest_framework.response import Response
+from rest_framework import status
+from modules.users.serializers import UserSerializer
+from modules.users.services import UserService
+
 
 # Create your views here.
+class UserCreateView(APIView):
+    def post(self, request):
+        try:
+            dto_data = UserCreateDto(**request.data)
+        except PydanticValidationError as e:
+            return Response({"errors": e.errors()}, status=status.HTTP_400_BAD_REQUEST)
+
+        new_user_dto = UserService.create_user(dto_data)
+        serializer = UserSerializer(data=new_user_dto.model_dump())
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
